9475ba9392c34e48d66c02175c442b4bf3c2f8aa
Nicholas <nicholas.bgp@gmail.com>
2019-07-24 22:04:36 -0600

    Including cython generated cpp files, MANIFEST.in added to force inclusion of header file, setup.py now directs compilation of cpp files, does not call cythonize, make_release.sh does not compile wheel as it's prohibited from linux distros using cpp.

--- make_release.sh
+++ make_release.sh
@@ -1,10 +1,11 @@
 
 # Remove any existing distribution archives
 rm -rf dist
+mkdir dist
 
 # Generate distribution archives
 python3 -m pip install --user --upgrade setuptools wheel
-python3 setup.py sdist bdist_wheel
+python3 setup.py sdist # bdist_wheel
 
 # Upload
 python3 -m pip install --user --upgrade twine
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 '''Setup.py'''
 
-from distutils.core import setup
-from distutils.extension import Extension
-from setuptools import find_packages
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
+# from distutils.core import setup
+# from distutils.extension import Extension
+# from setuptools import find_packages, Extension
+# from Cython.Distutils import build_ext
+# from Cython.Build import cythonize
+
+from setuptools import setup, Extension, find_packages
+
 
 import numpy as np
 
 setup(
     name='pygrappa',
-    version='0.3.1',
+    version='0.3.4',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
@@ -33,12 +36,16 @@ setup(
     ],
     python_requires='>=3.5',
 
-    # And now for some Cython...
-    cmdclass={'build_ext': build_ext},
-    ext_modules=cythonize([Extension(
-        'pygrappa.cgrappa',
-        sources=[
-            'src/cgrappa.pyx',
-            'src/get_sampling_patterns.cpp'],
-        include_dirs=[np.get_include()])]),
+    # # And now for some Cython...
+    # cmdclass={'build_ext': build_ext},
+    # ext_modules=cythonize([Extension(
+    #     'pygrappa.cgrappa',
+    #     sources=[
+    #         'src/cgrappa.pyx',
+    #         'src/get_sampling_patterns.cpp'],
+    #     include_dirs=[np.get_include()])]),
+    ext_modules=[Extension(
+        "pygrappa.cgrappa",
+        ["src/cgrappa.cpp", "src/get_sampling_patterns.cpp"],
+        include_dirs=['src/', np.get_include()])]
 )
