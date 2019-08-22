dad5fe5373623c25fdd1e578f66bf37e6ddb8e52
mckib2 <nicholas.bgp@gmail.com>
2019-08-22 11:52:52 -0600

    More progress on slice-grappa.  Need to check with Ed to see if I'm doing the right thing.

--- examples/basic_slicegrappa.py
+++ examples/basic_slicegrappa.py
@@ -2,23 +2,26 @@
 
 import numpy as np
 from phantominator import shepp_logan
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 from pygrappa import slicegrappa
 from utils import gaussian_csm
 
 if __name__ == '__main__':
 
-    # Get 2 slices of 3D Shepp-Logan phantom
+    # Get slices of 3D Shepp-Logan phantom
     N = 128
-    ph = shepp_logan((N, N, 2), zlims=(-.3, 0))
+    ns = 2
+    ph = shepp_logan((N, N, ns), zlims=(-.3, 0))
 
     # Apply some coil sensitivities
-    ncoil = 4
+    ncoil = 8
     csm = gaussian_csm(N, N, ncoil)
     ph = ph[..., None, :]*csm[..., None]
 
-    # Flip one of the slices so we can see it better
-    ph[..., -1] = np.rot90(ph[..., -1])
+    # Shift one slice FOV/2 (SMS-CAIPI)
+    ph[..., -1] = np.fft.fftshift(ph[..., -1], axes=0)
 
     # import matplotlib.pyplot as plt
     # for ii in range(ph.shape[-1]):
@@ -27,18 +30,46 @@ if __name__ == '__main__':
     # plt.show()
 
     # Put into kspace
+    ax = (0, 1)
     kspace = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(
-        ph, axes=(0, 1)), axes=(0, 1)), axes=(0, 1))
+        ph, axes=ax), axes=ax), axes=ax)
 
     # Calibration data is individual slices
     calib = kspace.copy()
 
     # Simulate SMS by simply adding slices together
-    kspace_sms = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(
-        np.sum(ph, axis=-1), axes=(0, 1)), axes=(0, 1)), axes=(0, 1))
+    kspace_sms = np.sum(kspace, axis=-1)
 
-    # We technically only have 1 time frame
-    kspace_sms = kspace_sms[..., None]
+    # Make identical time frames
+    nt = 5
+    kspace_sms = np.tile(kspace_sms[..., None], (1, 1, 1, nt))
 
     # Separate the slices using Slice-GRAPPA
-    slicegrappa(kspace_sms, calib)
+    res = slicegrappa(kspace_sms, calib, kernel_size=(5, 5))
+
+    # IFFT and stitch slices together
+    res = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(
+        res, axes=ax), axes=ax), axes=ax)
+    res0 = np.zeros((ns*N, N, nt))
+    for ii in range(ns):
+        res0[ii*N:(ii+1)*N, ...] = np.sqrt(
+            np.sum(np.abs(res[..., ii])**2, axis=2))
+
+    # Some code to look at the animation
+    fig = plt.figure()
+    ax = plt.imshow(np.abs(res0[..., 0]), cmap='gray')
+
+    def init():
+        '''Initialize ax data.'''
+        ax.set_array(np.abs(res0[..., 0]))
+        return(ax,)
+
+    def animate(frame):
+        '''Update frame.'''
+        ax.set_array(np.abs(res0[..., frame]))
+        return(ax,)
+
+    anim = FuncAnimation(
+        fig, animate, init_func=init, frames=nt,
+        interval=40, blit=True)
+    plt.show()
--- pygrappa/slicegrappa.py
+++ pygrappa/slicegrappa.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from skimage.util import view_as_windows, pad
+from tqdm import trange
 
 def slicegrappa(
         kspace, calib, kernel_size=(5, 5), coil_axis=-2,
@@ -19,6 +20,12 @@ def slicegrappa(
     kernel_size : tuple, optional
         Size of the GRAPPA kernel: (kx, ky).
 
+    Returns
+    -------
+    res : array_like
+        Reconstructed slices for each time frame.  res has fixed
+        shape: (nx, ny, num_coils, num_time_frames, num_slices).
+
     References
     ----------
     .. [1] Setsompop, Kawin, et al. "Blipped‐controlled aliasing in
@@ -30,9 +37,10 @@ def slicegrappa(
     # Put the axes where we expect them
     kspace = np.moveaxis(kspace, (coil_axis, time_axis), (-2, -1))
     calib = np.moveaxis(calib, (coil_axis, slice_axis), (-2, -1))
-    nx, ny, nc, _nt = kspace.shape[:]
+    nx, ny, nc, nt = kspace.shape[:]
     kx, ky = kernel_size[:]
     kx2, ky2 = int(kx/2), int(ky/2)
+    # adjx, adjy = np.mod(kx, 2), np.mod(ky, 2)
     _cx, _cy, nc, cs = calib.shape[:]
 
     # Pad kspace data
@@ -43,32 +51,38 @@ def slicegrappa(
         calib, ((kx2, kx2), (ky2, ky2), (0, 0), (0, 0)),
         mode='constant')
 
-    # Get all overlapping patches of single slice calibration data
-    A = view_as_windows(
-        calib, (kx, ky, nc, cs)).reshape((-1, kx, ky, nc, cs))
-
     # Source data from SMS simulated calibration data
-    calib_sms = np.fft.fft2(np.sum(np.fft.ifft2(
-        calib, axes=(0, 1)), -1), axes=(0, 1))
     S = view_as_windows(
-        calib_sms, (kx, ky, nc)).reshape((-1, kx*ky*nc))
+        np.sum(calib, axis=-1), (kx, ky, nc)).reshape((-1, kx*ky*nc))
 
     # Train a kernel for each target slice
-    res = np.zeros((nx, ny, nc, cs), dtype=kspace.dtype)
+    W = np.zeros((cs, S.shape[-1], nc), dtype=calib.dtype)
     for sl in range(cs):
 
         # Train GRAPPA kernel for the current slice
-        T = A[:, kx2, ky2, :, sl] # Target slice
+        T = calib[kx2:-kx2, ky2:-ky2, :, sl].reshape((-1, nc))
         ShS = S.conj().T @ S
         ShT = S.conj().T @ T
         lamda0 = lamda*np.linalg.norm(ShS)/ShS.shape[0]
-        W = np.linalg.solve(
+        W[sl, ...] = np.linalg.solve(
             ShS + lamda0*np.eye(ShS.shape[0]), ShT)
 
-        # Check to make sure it does what we think it's doing
-        res[..., sl] = (S @ W).reshape((nx, ny, nc))
-        import matplotlib.pyplot as plt
-        res0 = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(
-            res[..., sl], axes=(0, 1)), axes=(0, 1)), axes=(0, 1))
-        plt.imshow(np.abs(res0[..., 0]))
-        plt.show()
+        # # Check to make sure it does what we think it's doing
+        # res[..., sl] = (S @ W[sl, ...]).reshape((nx, ny, nc))
+        # import matplotlib.pyplot as plt
+        # res0 = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(
+        #     res[..., sl], axes=(0, 1)), axes=(0, 1)), axes=(0, 1))
+        # plt.imshow(np.sqrt(np.sum(np.abs(res0)**2, axis=-1)))
+        # plt.show()
+
+    # Now pull apart slices for each time frame
+    res = np.zeros((nx, ny, nc, nt, cs), dtype=kspace.dtype)
+    S = view_as_windows(
+        kspace, (kx, ky, nc, nt)).reshape((-1, kx*ky*nc, nt))
+    for tt in trange(nt, leave=False, desc='Slice-GRAPPA'):
+        for sl in range(cs):
+            res[..., tt, sl] = (
+                S[..., tt] @ W[sl, ...]).reshape((nx, ny, nc))
+
+    # Return results in fixed order: (nx, ny, nc, nt, cs)
+    return res
