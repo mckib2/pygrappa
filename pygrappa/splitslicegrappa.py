5b5e2e51c024512d6b1448cb88670052975440f7
mckib2 <nicholas.bgp@gmail.com>
2019-08-28 16:48:21 -0600

    Add alias for split-slice-grappa implementation

---
+++ pygrappa/splitslicegrappa.py
@@ -0,0 +1,20 @@
+'''Python implementation of Split-Slice-GRAPPA.'''
+
+from pygrappa import slicegrappa
+
+def splitslicegrappa(*args, **kwargs):
+    '''Split-Slice-GRAPPA.
+
+    Notes
+    -----
+    This is an alias for pygrappa.slicegrappa(split=True).
+    See pygrappa.slicegrappa() for more information.
+    '''
+
+    # Make sure that the 'split' argument is set to True
+    if 'split' not in kwargs or not kwargs['split']:
+        kwargs['split'] = True
+    return slicegrappa(*args, **kwargs)
+
+if __name__ == '__main__':
+    pass
