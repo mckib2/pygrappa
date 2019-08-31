f86a5d9c9f42262cc10ce4b2bcbe2f340b144243
Nicholas <nicholas.bgp@gmail.com>
2019-08-31 16:51:33 -0600

    Bump version ahead of VC-GRAPPA release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class build_ext(_build_ext):
 
 setup(
     name='pygrappa',
-    version='0.6.0',
+    version='0.7.0',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
