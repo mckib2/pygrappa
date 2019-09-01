bcf619279b1a5965b21a3cf692c3ed1b27cae692
Nicholas <nicholas.bgp@gmail.com>
2019-09-01 13:41:50 -0600

    iGRAPPA now performs better than just GRAPPA.  Might deviate from paper, but I can't figure out what the paper wants me to do exactly...

--- pygrappa/igrappa.py
+++ pygrappa/igrappa.py
@@ -1,15 +1,54 @@
 '''Python implementation of the iGRAPPA algorithm.'''
 
 import numpy as np
+from tqdm import trange
+from skimage.measure import compare_mse
 
 from pygrappa import cgrappa
 
-def igrappa(kspace, calib, k=0.6, coil_axis=-1):
+def igrappa(
+        kspace, calib, kernel_size=(5, 5), k=0.3, coil_axis=-1,
+        lamda=0.01, ref=None, niter=10, silent=True):
     '''Iterative GRAPPA.
+
+    Parameters
+    ----------
+    kspace : array_like
+        2D multi-coil k-space data to reconstruct from.  Make sure
+        that the missing entries have exact zeros in them.
+    calib : array_like
+        Calibration data (fully sampled k-space).
+    kernel_size : tuple, optional
+        Size of the 2D GRAPPA kernel (kx, ky).
+    k : float, optional
+        Regularization parameter for iterative reconstruction.  Must
+        be in the interval (0, 1).
+    coil_axis : int, optional
+        Dimension holding coil data.  The other two dimensions should
+        be image size: (sx, sy).
+    lamda : float, optional
+        Tikhonov regularization for the kernel calibration.
+    ref : array_like or None, optional
+        Reference k-space data.  This is the true data that we are
+        attempting to reconstruct.  If provided, MSE at each
+        iteration will be returned.  If None, only reconstructed
+        kspace is returned.
+    niter : int, optional
+        Number of iterations.
+    silent : bool, optional
+        Suppress messages to user.
+
+    Returns
+    -------
+
+    Raises
+    ------
+    AssertionError
+        If regularization parameter k is not in the interval (0, 1).
 
     Notes
     -----
-    Implements the two-stage algorithm described in [1].
+    More or less implements the iterative algorithm described in [1].
 
     References
     ----------
@@ -20,47 +59,81 @@ def igrappa(kspace, calib, k=0.6, coil_axis=-1):
            59.4 (2008): 903-907.
     '''
 
-    # Make sure 
-    assert 0 < k < 1, 'k should be in (0, 1)!'
+    # Make sure k has a reasonable value
+    assert 0 < k < 1, 'Parameter k should be in (0, 1)!'
+
+    # Collect arguments to pass to cgrappa:
+    grappa_args = {
+        'kernel_size': kernel_size,
+        'coil_axis': -1,
+        'lamda': lamda,
+        'silent': silent
+    }
 
     # Put the coil dimension at the end
     kspace = np.moveaxis(kspace, coil_axis, -1)
     calib = np.moveaxis(calib, coil_axis, -1)
-
-    # Stage 1: "Using calibration data to obtain the initial weights,
-    # followed by iterative GRAPPA reconstruction with the
-    # restriction that only continuously measured lines in the
-    # middle of k‐space are used on the left side of Eq. [1]."
+    kx, ky, _nc = kspace.shape[:]
+    cx, cy, _nc = calib.shape[:]
+    kx2, ky2 = int(kx/2), int(ky/2)
+    cx2, cy2 = int(cx/2), int(cy/2)
 
     # Initial conditions
-    Im, W = cgrappa(kspace, calib, coil_axis=-1, ret_weights=True)
-    Fp = 1e6
+    kIm, W = cgrappa(kspace, calib, ret_weights=True, **grappa_args)
+    ax = (0, 1)
+    Im = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(
+        kIm, axes=ax), axes=ax), axes=ax)
+    Fp = 1e6 # some large number to begin with
 
-    niter = 4
-    for _ii in range(niter):
+    # If user has provided reference, let's track the MSE
+    if ref is not None:
+        mse = np.zeros(niter)
+        aref = np.abs(ref)
 
-        Tm, Wn = cgrappa(
-            kspace, calib, coil_axis=-1, ret_weights=True)
+    # Fixed number of iterations
+    for ii in trange(niter, leave=False, desc='iGRAPPA'):
+
+        # Update calibration region -- now includes all estimated
+        # lines plus unchanged calibration region
+        calib0 = kIm.copy()
+        calib0[kx2-cx2:kx2+cx2, ky2-cy2:ky2+cy2, :] = calib.copy()
+
+        kTm, Wn = cgrappa(
+            kspace, calib0, ret_weights=True, **grappa_args)
+        Tm = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(
+            kTm, axes=ax), axes=ax), axes=ax)
 
         # Estimate relative image intensity change
-        Tp = np.sum((np.abs(Tm) - np.abs(Im)).flatten())
-        Tp /= np.sum(np.abs(Im).flatten())
+        l1_Tm = np.linalg.norm(Tm.flatten(), ord=1)
+        l1_Im = np.linalg.norm(Im.flatten(), ord=1)
+        Tp = np.abs(l1_Tm - l1_Im)/l1_Im
 
         # Update weights
         p = Tp/(k*Fp)
         if p < 1:
             # Take this reconstruction and new weights
             Im = Tm
+            kIm = kTm
             W = Wn
         else:
             # Modify weights to get new reconstruction
             p = 1/p
-            W = (1 - p)*Wn + p*W
+            W = [(1 - p)*Wn0 + p*W0 for Wn0, W0 in zip(Wn, W)]
 
             # Need to be able to supply grappa with weights to use!
-            # Im = grappa_with_supplied_weights(W)
+            kIm = cgrappa(kspace, calib0, Wsupp=W, **grappa_args)
+            Im = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(
+                kIm, axes=ax), axes=ax), axes=ax)
 
         # Update Fp
         Fp = Tp
 
-        assert False
+        # Track MSE
+        if ref is not None:
+            mse[ii] = compare_mse(aref, np.abs(kIm))
+
+    # Return the reconstructed kspace and MSE if ref kspace provided,
+    # otherwise, just return reconstruction
+    if ref is not None:
+        return(kIm, mse)
+    return kIm
