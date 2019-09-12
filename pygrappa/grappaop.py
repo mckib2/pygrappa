d99c43fd3b9cc23374878b6aed9fd2a34732db9e
mckib2 <nicholas.bgp@gmail.com>
2019-09-11 20:19:22 -0600

    GRAPPA operator now seems to be working, added Tikhonov regularization to match how cgrappa trains weights

--- pygrappa/grappaop.py
+++ pygrappa/grappaop.py
@@ -2,18 +2,32 @@
 
 import numpy as np
 
-def grappaop(calib, coil_axis=-1):
+def grappaop(calib, coil_axis=-1, lamda=0.01):
     '''GRAPPA operator.
 
     Parameters
     ----------
+    calib : array_like
+        Calibration region data.  Usually a small portion from the
+        center of kspace.
+    coil_axis : int, optional
+        Dimension holding coil data.
+    lamda : float, optional
+        Tikhonov regularization parameter.  Set to 0 for no
+        regularization.
 
     Returns
     -------
+    Gx, Gy : array_like
+        GRAPPA operators for both the x and y directions.
 
     Notes
     -----
-    Produces the infinitesimal operator described in [1]_.
+    Produces the unit operator described in [1]_.
+
+    This seems to only work well when coil sensitivities are very
+    well separated/distinct.  If coil sensitivities are similar,
+    operators perform poorly.
 
     References
     ----------
@@ -26,20 +40,23 @@ def grappaop(calib, coil_axis=-1):
     calib = np.moveaxis(calib, coil_axis, -1)
     _cx, _cy, nc = calib.shape[:]
 
-    # consider a single dimension of calibration data:
-    #     T = S G
-    #     S^H T = S^H S G
-    #     (S^H S)^-1 S^H T = G
+    # We need sources (last source has no target!)
+    Sx = np.reshape(calib[:-1, ...], (-1, nc))
+    Sy = np.reshape(calib[:, :-1, :], (-1, nc))
 
-    # We need sources and targets
-    dim = 'C' # use 'F' for other direction
-    S = np.reshape(calib, (-1, nc), order=dim)
+    # And we need targets for an operator along each axis (first
+    # target has no associated source!)
+    Tx = np.reshape(calib[1:, ...], (-1, nc))
+    Ty = np.reshape(calib[:, 1:, :], (-1, nc))
 
-    # Could be (0, 1, 0)?
-    T = np.roll(calib, (1, 0, 0)).reshape((-1, nc), order=dim)
+    # Train the operators:
+    Sxh = Sx.conj().T
+    lamda0 = lamda*np.linalg.norm(Sxh)/Sxh.shape[0]
+    Gx = np.linalg.solve(
+        Sxh @ Sx + lamda0*np.eye(Sxh.shape[0]), Sxh @ Tx)
 
-    ShS = S.conj().T @ S
-    ShT = S.conj().T @ T
-    G = np.linalg.solve(ShS, ShT)
-
-    return G
+    Syh = Sy.conj().T
+    lamda0 = lamda*np.linalg.norm(Syh)/Syh.shape[0]
+    Gy = np.linalg.solve(
+        Syh @ Sy + lamda0*np.eye(Syh.shape[0]), Syh @ Ty)
+    return(Gx, Gy)
