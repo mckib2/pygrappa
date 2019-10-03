76b81fa5dcae8936c2ad177715337c58ab9279b0
mckib2 <nicholas.bgp@gmail.com>
2019-10-02 22:06:02 -0600

    Add simple example showing how to get grappa operators from radial data

--- pygrappa/examples/basic_radialgrappaop.py
+++ pygrappa/examples/basic_radialgrappaop.py
@@ -1,19 +1,28 @@
 '''Basic usage of Radial GRAPPA operator.'''
 
+from time import time
+
 import numpy as np
+import matplotlib.pyplot as plt
 from phantominator import radial, kspace_shepp_logan
 
 from pygrappa import radialgrappaop
 
 if __name__ == '__main__':
 
-    N, spokes, nc = 64, 32, 8
+    N, spokes, nc = 288, 62, 3
     kx, ky = radial(N, spokes)
-    kx = np.reshape(kx, (N, spokes)).flatten('F')
-    ky = np.reshape(ky, (N, spokes)).flatten('F')
+    kx = np.reshape(kx, (N, spokes), 'F').flatten()
+    ky = np.reshape(ky, (N, spokes), 'F').flatten()
     k = kspace_shepp_logan(kx, ky, ncoil=nc)
     k = np.reshape(k, (N, spokes, nc))
     kx = np.reshape(kx, (N, spokes))
     ky = np.reshape(ky, (N, spokes))
 
+    plt.scatter(kx, ky, .1)
+    plt.title('Radial Sampling Pattern')
+    plt.show()
+
+    t0 = time()
     Gx, Gy = radialgrappaop(kx, ky, k)
+    print('Gx, Gy computed in %g seconds' % (time() - t0))
