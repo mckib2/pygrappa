1e9caa92f6dc0da835ba22107c27ea70cf08392b
Nicholas <nicholas.bgp@gmail.com>
2019-10-12 15:10:29 -0600

    Bumpy version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class build_ext(_build_ext):
 
 setup(
     name='pygrappa',
-    version='0.13.0',
+    version='0.13.1',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
