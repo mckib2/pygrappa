69f4c8cc7f8a8cbce057da15c45905f9d116600d
mckib2 <nicholas.bgp@gmail.com>
2019-11-07 21:13:17 -0700

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class build_ext(_build_ext):
 
 setup(
     name='pygrappa',
-    version='0.13.2',
+    version='0.14.0',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
