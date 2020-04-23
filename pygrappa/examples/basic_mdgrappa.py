00aa45ed040c463d591c06fc6cebf6bed8f0c506
mckib2 <nicholas.bgp@gmail.com>
2020-04-22 21:19:10 -0600

    Raise NotImplementedError for find_acs

---
+++ pygrappa/examples/basic_mdgrappa.py
@@ -0,0 +1,56 @@
+'''Basic usage of multidimensional GRAPPA.'''
+
+from time import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+from phantominator import shepp_logan
+
+from pygrappa import mdgrappa
+from utils import gaussian_csm
+
+if __name__ == '__main__':
+
+    # Generate fake sensitivity maps: mps
+    L, M, N = 128, 128, 32
+    ncoils = 4
+    mps = gaussian_csm(L, M, ncoils)[..., None, :]
+
+    # generate 3D phantom
+    ph = shepp_logan((L, M, N), zlims=(-.25, .25))
+    imspace = ph[..., None]*mps
+    ax = (0, 1, 2)
+    kspace = np.fft.fftshift(np.fft.fftn(
+        np.fft.ifftshift(imspace, axes=ax), axes=ax), axes=ax)
+
+    # crop 20x20xN window from the center of k-space for calibration
+    # (use all z-axis)
+    pd = 10
+    ctrs = [int(s/2) for s in kspace.shape[:2]]
+    calib = kspace[tuple([slice(ctr-pd, ctr+pd) for ctr in ctrs] + [slice(None), slice(None)])].copy()
+    #calib = kspace[ctr-pd:ctr+pd, ctr-pd:ctr+pd, :].copy()
+
+    # calibrate a kernel
+    kernel_size = (4, 5, 5)
+
+    # undersample by a factor of 2 in both kx and ky
+    kspace[::2, 1::2, ...] = 0
+    kspace[1::2, ::2, ...] = 0
+
+    # Do the recon
+    t0 = time()
+    res = mdgrappa(kspace, calib, kernel_size)
+    print('Took %g sec' % (time() - t0))
+
+    # Take a look at a single slice (z=-.25)
+    res = np.abs(np.fft.fftshift(np.fft.ifftn(
+        np.fft.ifftshift(res, axes=ax), axes=ax), axes=ax))
+    res = res[..., 0, :]
+    res0 = np.zeros((2*L, 2*M))
+    kk = 0
+    for idx in np.ndindex((2, 2)):
+        ii, jj = idx[:]
+        res0[ii*L:(ii+1)*L, jj*M:(jj+1)*M] = res[..., kk]
+        kk += 1
+    plt.imshow(res0, cmap='gray')
+    plt.show()
--- pygrappa/mdgrappa.py
+++ pygrappa/mdgrappa.py
@@ -6,54 +6,6 @@ import numpy as np
 from skimage.util import view_as_windows
 from tqdm import tqdm
 
-from utils import findRectangle2d
-
-def _find_acs(kspace):
-    '''Start at center of kspace and find largest hyper-rectangle.
-
-    Parameters
-    ----------
-    kspace : N-D array
-        Assume coil_axis is at -1 and currently unpadded.
-    '''
-    from skimage.segmentation import flood_fill
-    #import matplotlib.pyplot as plt
-
-    # Flood fill from center
-    mask = np.abs(kspace[..., 0]) > 0
-    ctr = tuple([sh//2 for sh in kspace.shape[:-1]])
-    if mask[ctr] == 0:
-        raise ValueError('There is no sample at the center!')
-    ACS_val = 2
-    region = flood_fill(mask.astype(int), seed_point=ctr, new_value=ACS_val, connectivity=0) == ACS_val
-
-    #plt.imshow(region)
-    #plt.show()
-
-    if region.ndim == 2:
-
-        # Find a centered rectangle
-        acs, top, bottom = findRectangle2d(region, mask, ctr)
-
-        #plt.imshow(acs)
-        #plt.show()
-
-        nc = kspace.shape[-1]
-        acs = np.tile(acs[..., None], (1, 1, nc))
-        # I'm not sure what I'm doing wrong yet...
-        for ii in range(3):
-            try:
-                calib = kspace[acs].reshape((bottom-top-ii, -1, nc))
-                break
-            except ValueError:
-                pass
-        else:
-            raise ValueError()
-    else:
-        raise NotImplementedError()
-
-    return calib
-
 def mdgrappa(
         kspace,
         calib=None,
@@ -125,7 +77,8 @@ def mdgrappa(
         calib = np.moveaxis(calib, coil_axis, -1)
     else:
         # Find the calibration region and split it out from kspace
-        calib = _find_acs(kspace)
+        raise NotImplementedError("Auto ACS extraction not implemented!")
+        #calib = find_acs(kspace)
 
     # Pad the arrays
     pads = [int(k/2) for k in kernel_size]
