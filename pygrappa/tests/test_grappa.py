ec9b6c868b2c611c60a0e3fb082c22c5d6e01a56
mckib2 <nicholas.bgp@gmail.com>
2020-04-24 21:15:51 -0600

    basic GRAPPA tests

---
+++ pygrappa/tests/test_grappa.py
@@ -0,0 +1,12 @@
+'''Unit tests for GRAPPA.'''
+
+import unittest
+
+from pygrappa import grappa
+from .helpers import make_base_test_case_2d
+
+class TestGRAPPA(make_base_test_case_2d(grappa, ssim_thresh=.92)):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
