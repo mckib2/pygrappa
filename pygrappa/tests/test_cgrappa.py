8750ffa591a71aff68c15f2a2747f364adf9891a
mckib2 <nicholas.bgp@gmail.com>
2020-04-24 21:20:36 -0600

    add cgrappa tests

---
+++ pygrappa/tests/test_cgrappa.py
@@ -0,0 +1,13 @@
+'''Unit tests for CGRAPPA.'''
+
+import unittest
+
+import numpy as np
+from pygrappa import cgrappa
+from .helpers import make_base_test_case_2d
+
+class TestCGRAPPA(make_base_test_case_2d(cgrappa, ssim_thresh=.92, types=[('complex128', np.complex128)])):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
