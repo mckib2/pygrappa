7ee3c70ab6c5b93efcc2dc7689d1361931779c5e
mckib2 <nicholas.bgp@gmail.com>
2020-04-25 18:06:53 -0600

    tell sphinx to look for index.rst instead of contents.rst

--- docs/conf.py
+++ docs/conf.py
@@ -23,11 +23,15 @@ author = 'Nicholas McKibben'
 
 # -- General configuration ---------------------------------------------------
 
+# Make sure ReadTheDocs doesn't choke on C extensions
 autodoc_mock_imports = [
     'pygrappa.cgrappa',
     'pygrappa.grog_powers',
     'pygrappa.grog_gridding',
 ]
+
+# Point to index instead of contents.rst
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -39,7 +43,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
 ]
-
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,10 +58,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'references.rst']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-#html_theme = 'python_docs_theme'
 html_theme = 'sphinx_rtd_theme'
-#html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
