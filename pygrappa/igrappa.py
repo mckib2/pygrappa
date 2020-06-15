9f56e522d40178bf49017a3dff0ece5be07afb01
mckib2 <nicholas.bgp@gmail.com>
2020-06-14 20:16:52 -0600

    Refactor weight update to hopefully resolve python 3.5 issue

--- pygrappa/igrappa.py
+++ pygrappa/igrappa.py
@@ -148,7 +148,8 @@ def igrappa(
         else:
             # Modify weights to get new reconstruction
             p = 1/p
-            W = {key: (1 - p)*Wn0 + p*W0 for (key, Wn0), (_, W0) in zip(Wn.items(), W.items())}
+            for key in Wn:
+                W[key] = (1 - p)*Wn[key] + p*W[key]
 
             # Need to be able to supply grappa with weights to use!
             kIm = backend(kspace, calib0, weights=W, **grappa_args)
