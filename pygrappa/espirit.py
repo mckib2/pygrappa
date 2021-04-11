2fbbfdb364b0bfb74d120dd78b96edc596130519
Nicholas McKibben <nicholas.bgp@gmail.com>
2021-04-10 22:05:17 -0700

    ENH: fix FFT loop and resolve ordering issues

--- pygrappa/espirit.py
+++ pygrappa/espirit.py
@@ -17,8 +17,9 @@ logging.basicConfig(level=logging.INFO)
 
 def _fft(x, s=None, axes=None):
     return np.fft.fftshift(
-        np.fft.fftn(np.fft.ifftshift(
-            x, axes=axes), s=s, axes=axes), axes=axes)
+        np.fft.fft2(
+            np.fft.ifftshift(x, axes=axes),
+            s=s, axes=axes), axes=axes)
 
 
 def espirit(kspace, calib, kernel_size=None, eig_thresh_1=0.02, eig_thresh_2=0.95, coil_axis=-1):
@@ -55,47 +56,58 @@ def espirit(kspace, calib, kernel_size=None, eig_thresh_1=0.02, eig_thresh_2=0.9
     v = np.conj(vh).T
     logger.info('Second SVD size %s took %g seconds',
                 k.shape, time()-t0)
-    k = k @ v
+    k = np.einsum('ij,jk->ik', k, v)
     k = np.reshape(k, (kx, ky, nv, nc))
     k = np.moveaxis(k, 3, 2)
     ax = (0, 1)
     t0 = time()
-    k = _fft(np.conj(k[::-1, ::-1, ...])*np.sqrt(sx*sy), s=(sx, sy), axes=ax)/np.sqrt(kx*ky)
+    print(k.shape)
+    # k = _fft(np.conj(k[::-1, ::-1, ...]), s=(sx, sy), axes=ax)/np.sqrt(kx*ky)  # TODO: why is this not equivalent to following?
+    xpd = sx // 2 - kx // 2
+    ypd = sy // 2 - ky // 2  # TODO: might need to consider odd/even cases
+    KERNEL = np.zeros((sx, sy, k.shape[2], k.shape[3]), dtype=k.dtype)
+    for n in range(k.shape[3]):
+        KERNEL[..., n] = _fft(
+            np.pad(np.conj(k[::-1, ::-1, :, n]), ((xpd, xpd), (ypd, ypd), (0, 0))),
+            axes=ax)
+    KERNEL /= np.sqrt(kx*ky)
+    k = KERNEL
     logger.info('FFT took %g seconds', time()-t0)
-
+    
     t0 = time()
     c, d, _vh = np.linalg.svd(k, full_matrices=False, compute_uv=True)
     logger.info('Third SVD size %s took %g seconds',
                 k.shape, time()-t0)
-    ph = np.exp(-1j*np.angle(c[0, :]))[None, :]
-    M = v @ c*ph
-    W = np.real(d)
-
+    ph = np.exp(-1j*np.angle(c[..., 0, :]))[..., None, :]  # TODO: something fishy is happening here -- phase of M doesn't look like MATLAB output
+    M = np.einsum('ij,xyjk->xyik', v, c*ph)[..., ::-1]
+    W = np.real(d)[..., ::-1]
+    
     print('M is', M.shape)
 
     import matplotlib.pyplot as plt
 
     idx = 1
-    for ii in range(nc):
-        for jj in range(min(nc, nv)):
-            plt.subplot(nc, nc, idx)
-            plt.imshow(np.abs(M[..., ii, jj]))
-            idx += 1
-    plt.show()
-
-    idx = 1
-    for ii in range(nc):
-        for jj in range(min(nc, nv)):
-            plt.subplot(nc, nc, idx)
-            plt.imshow(np.angle(M[..., ii, jj]))
-            idx += 1
-    plt.show()
-
-    idx = 1
     for ii in range(min(nc, nv)):
         plt.subplot(1, nc, idx)
-        plt.imshow(W[..., ii])
+        plt.imshow(np.abs(W[..., ii]))
         idx += 1
+    plt.title('W')
+    plt.show()
+    
+    idx = 1
+    for ii in range(nc):
+        for jj in range(min(nc, nv)):
+            plt.subplot(nc, nc, idx)
+            plt.imshow(np.abs(M[..., jj, ii]))
+            idx += 1
+    plt.show()
+
+    idx = 1
+    for ii in range(nc):
+        for jj in range(min(nc, nv)):
+            plt.subplot(nc, nc, idx)
+            plt.imshow(np.angle(M[..., jj, ii]))
+            idx += 1
     plt.show()
 
 
@@ -104,7 +116,7 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from scipy.io import loadmat
     # from pygrappa import find_acs
-    matdata = loadmat('/home/nicholas/Downloads/SPIRiT_v0.3/ESPIRiT/data/brain_8ch.mat')
+    matdata = loadmat('/home/nmckibben/Documents/SPIRiT_v0.3/ESPIRiT/data/brain_8ch.mat')
     data = matdata['DATA']
     mask = matdata['mask_unif']
     # data = data*mask[..., None]
