6dd776a4ca987a6909bd0dff4db77ebd24f3fbef
Nicholas McKibben <nicholas.bgp@gmail.com>
2022-11-16 21:39:53 -0700

    CI: correct escape replacement

--- patch_mesonpep517.py
+++ patch_mesonpep517.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     buildapi = pathlib.Path(mesonpep517.__file__).parent / "buildapi.py"
     with open(buildapi, "r") as fp:
         contents = fp.read()
-    contents = contents.replace(".decode('utf-8').strip('\n')", ".decode('utf-8').strip('\n\r')")
+    contents = contents.replace(".decode('utf-8').strip('\\n')", ".decode('utf-8').strip('\\n\\r')")
     contents = contents.replace("abi = get_abi(python)", "abi = get_abi(sys.executable)")
     with open(buildapi, "w") as fp:
         fp.write(contents)
+    print("Patched buildapi.py for Windows!")
